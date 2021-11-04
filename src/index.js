81f1ad12a12a81f9f6cdb722379f4e343d2611a7
王聪 <cong_wa@163.com>
2021-11-04 21:14:05 +0800

    源码注释

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@
 
 /* eslint-disable no-console, fecs-no-require */
 
+// webpackplugin中间的apihttps://webpack.docschina.org/api/compilation-hooks/
+// webpack2手册
+
 const ssr = require('./ssr');
 const {insertAt, isObject} = require('./util');
 
@@ -21,8 +24,7 @@ class SkeletonPlugin {
         this.options = Object.assign({}, DEFAULT_PLUGIN_OPTIONS, options);
     }
 
-    apply(compiler) {
-
+    apply(compiler) { // Compiler代表了是整个webpack从启动到关闭的生命周期
         let {webpackConfig, insertAfter} = this.options;
         let entry = webpackConfig.entry;
         // cache entries
@@ -42,17 +44,57 @@ class SkeletonPlugin {
                 [entryName]: entry
             };
         }
+        
+        /**
+         * 
+         * 编译实例继承于编译器。
+         * 例如，compiler.compilation 是对所有 require 图表中对象的字面上的编译。
+         * 这个对象可以访问所有的模块和它们的依赖（大部分是循环依赖）。
+         * 在编译阶段，模块被加载，封闭，优化，分块，哈希和重建等等。这将是编译中任何操作主要的生命周期。
+         * 
+         * Compilation 对象只代表了一次新的编译
+         * 
+         * 其常见钩子
+         *  after-plugins     设置完一组初始化插件之后    compiler          sync
+            after-resolvers   设置完 resolvers 之后     compiler          sync
+            run               在读取记录之前             compiler          async
+            compile           在创建新 compilation之前  compilationParams  sync
+            compilation       compilation 创建完成      compilation        sync
+            emit              在生成资源并输出到目录之前  compilation        async
+            after-emit        在生成资源并输出到目录之后  compilation        async
+            done              完成编译                  stats              sync
 
+
+            watch-run 获取发生变换的文件列表
+         * 
+         */
         compiler.plugin('compilation', compilation => {
-
+            // compilation创建完成
             // add listener for html-webpack-plugin
-            compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
+            // 监听html-webpack-plugin的html-webpack-plugin-before-html-processing生命周期
 
+            /**
+             * webpack的事件流
+             * 
+             * 我们可以把webpack理解为一条生产线，需要经过一系列处理流程后才能将源文件转换成输出结果。
+             * 这条生产线上的每个处理流程的职责都是单一的，多个流程之间会存在依赖关系，只有完成当前处理后才能交给下一个流程去处理。
+             * webpack在运行的过程中会广播事件，插件只需要关心监听它的事件，就能加入到这条生产线中。然后会执行相关的操作。
+             * webpack的事件流机制它能保证了插件的有序性，使整个系统的扩展性好。事件流机制使用了观察者模式来实现的
+             * 
+             * 在html-webpack-plugin插件中通过compilation.applyPluginsAsyncWaterfall('html-webpack-plugin-before-html-processing', htmlPluginData); 进行广播 
+             */
+            // html-webpack-plugin处理html前触发
+            compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
+                // 拿到所有的文件数组
+
+                // 这里引发一个思考，如果不在htmlPluginData中拿chunk，还能怎么拿？ 
+                // compilation中的assets也是可以的
                 let usedChunks = Object.keys(htmlPluginData.assets.chunks);
                 let entryKey;
 
                 // find current processing entry
                 if (Array.isArray(usedChunks)) {
+                    // 找到我们的骨架屏模板文件地址
                     entryKey = Object.keys(skeletonEntries).find(v => usedChunks.indexOf(v) > -1);
                 }
                 else {
@@ -60,15 +102,27 @@ class SkeletonPlugin {
                 }
 
                 // set current entry & output in webpack config
+                // 设置webpack的入口
                 webpackConfig.entry = skeletonEntries[entryKey];
+                // 设置webpack出口地址
                 webpackConfig.output.filename = `skeleton-${entryKey}.js`;
 
+                // 走ssr流程，拿到ssr转换后的 html静态文件字符串和css字符串  之后就是插入了
+                /**
+                 * 这里仔细想一下如何进行插入。 我们的目的是把我们骨架屏的css和逻辑源码插入到html中，刚好html-webpack-plugin是往html插入文件的
+                 * 我们可以再html-webpack-plugin的关键地方插入我们的骨架屏css和js代码
+                 * 
+                 */
                 ssr(webpackConfig).then(({skeletonHtml, skeletonCss}) => {
                     // insert inlined styles into html
+
+                    //在html-webpack-plugin的上下文中找出 <head> 标签位置
                     let headTagEndPos = htmlPluginData.html.lastIndexOf('</head>');
+
+                    // 在head标签中插入 style代码
                     htmlPluginData.html = insertAt(htmlPluginData.html, `<style>${skeletonCss}</style>`, headTagEndPos);
 
-                    // replace mounted point with ssr result in html
+                    // 根据insertAfter关键字找出位置，插入script标签
                     let appPos = htmlPluginData.html.lastIndexOf(insertAfter) + insertAfter.length;
                     htmlPluginData.html = insertAt(htmlPluginData.html, skeletonHtml, appPos);
                     callback(null, htmlPluginData);
--- src/ssr.js
+++ src/ssr.js
@@ -10,6 +10,14 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const createBundleRenderer = require('vue-server-renderer').createBundleRenderer;
+
+/**
+ *  内存文件系统是在内存中模拟一个磁盘操作系统，因为读取磁盘速度比读取内存慢得多，
+ *  所以在需要频繁读写文件场景下，我们可以使用内存文件系统做为存储介质。例如在webpack中，
+ *  开发环境下就是将打包出来的bundle写如内存中实现快速存取
+ * 
+ * 
+ * */ 
 const MFS = require('memory-fs');
 
 module.exports = serverWebpackConfig => new Promise((resolve, reject) => {
@@ -22,6 +30,7 @@ module.exports = serverWebpackConfig => new Promise((resolve, reject) => {
     console.log(`Generate skeleton for ${outputBasename}...`);
 
     // extract css into a single file
+    // 拆解出**.vue模板文件的css命名为outputCssBasename
     serverWebpackConfig.plugins.push(new ExtractTextPlugin({
         filename: outputCssBasename
     }));
@@ -29,8 +38,12 @@ module.exports = serverWebpackConfig => new Promise((resolve, reject) => {
     // webpack start to work
     let serverCompiler = webpack(serverWebpackConfig);
     let mfs = new MFS();
+
     // output to mfs
+    // 控制把webpack的输出(配置的output)文件输出到内存中
     serverCompiler.outputFileSystem = mfs;
+
+    // 使用webpack的watch来进行热更新的功能呢
     serverCompiler.watch({}, (err, stats) => {
 
         if (err) {
@@ -46,11 +59,17 @@ module.exports = serverWebpackConfig => new Promise((resolve, reject) => {
             console.warn(err);
         });
 
+        // 构建的骨架屏模板js代码包
         let bundle = mfs.readFileSync(outputPath, 'utf-8');
+
+        // 构建的css文件
         let skeletonCss = mfs.readFileSync(outputCssPath, 'utf-8');
         // create renderer with bundle
+
+        // 给vue-server-renderer插件传入bundle
         let renderer = createBundleRenderer(bundle);
         // use vue ssr to render skeleton
+        // 使用vue的ssr进行解析转换拿到html静态标签代码
         renderer.renderToString({}, (err, skeletonHtml) => {
             if (err) {
                 reject(err);
